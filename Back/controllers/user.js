6861e8f183f0d9b1c834bef061d6981fad1ec465
Juliana Breda <julibredagomes@gmail.com>
2022-10-07 17:48:14 +0200

    added route login

--- Back/controllers/user.js
+++ Back/controllers/user.js
@@ -14,15 +14,26 @@ exports.signup = (req, res, next) => { //Save the new model 'user' to the data b
   })
 
   .catch(error => res.status(500).json({error}));
-
 };
-  
-
-exports.login = (req, res, next) => { //Save the new model 'user' to the data base MongoDB
-    delete req.body._id; // exclude the field ID to the collection saved to the date base Mongo DB
+     
     
-    User.findOne({ email: req.body.email, password: req.body.password })
-    .then(user => res.status(200).json(user))
-    .catch(error => res.status(404).json({ error }));
-
-  }
+exports.login = (req, res, next) => { //checks the data base MongoDB to find the user
+  User.findOne({ email: req.body.email })
+    .then(user => {
+        if (!user) {
+          return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+      }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+              if (!valid) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                      }
+              res.status(200).json({
+                  userId: user._id,
+                  token: 'TOKEN'
+              });
+            })
+            .catch(error => res.status(500).json({ error }));
+          })
+      .catch(error => res.status(500).json({ error }));
+   };
